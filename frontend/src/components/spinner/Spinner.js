8947d7baf531409f9d753b456a3f1ebee8162447
Kevin <kakurineuin@gmail.com>
2019-01-07 06:45:37 +0800

    實作完成 spinner。

--- frontend/src/components/spinner/Spinner.js
+++ frontend/src/components/spinner/Spinner.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import classes from "./spinner.css";
+import classes from "./spinner.module.css";
 
 class Spinner extends Component {
   render() {
     return (
-      // <div className={this.props.loading ? "modal is-active" : "modal"}>
-      //   <div className="modal-background" />
-      //   <div className="modal-content">
-      <div className={classes.Loader}>Loading...</div>
-      //   </div>
-      // </div>
+      <div className={this.props.loading ? "modal is-active" : "modal"}>
+        <div className="modal-background" />
+        <div
+          className="modal-content has-text-centered"
+          style={{ height: "100px", width: "100px" }}
+        >
+          <div className={classes.LdsRoller}>
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- handler/forum.go
+++ handler/forum.go
@@ -4,6 +4,8 @@ import (
 	"github.com/kakurineuin/golang-forum/service"
 	"github.com/labstack/echo"
 	"net/http"
+
+	"time"
 )
 
 // ForumHandler 處理論壇相關功能請求的 handler。
@@ -18,6 +20,8 @@ func (h ForumHandler) FindForumStatistics(c echo.Context) (err error) {
 	if err != nil {
 		return
 	}
+
+	time.Sleep(5 * time.Second)
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"forumStatistics": forumStatistics,
