443ba44b43cdd2ba111c3f9764b05ac2751838b1
Kevin <kakurineuin@gmail.com>
2019-01-20 17:42:46 +0800

    文章編輯器加上可以使用圖片網址的功能。

--- frontend/src/components/PostEditor.js
+++ frontend/src/components/PostEditor.js
@@ -6,32 +6,50 @@ import "react-quill/dist/quill.snow.css";
   文章編輯器元件。
 */
 class PostEditor extends Component {
-  modules = {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ["bold", "italic", "underline", "strike", "blockquote", "code-block"],
-      [
-        { list: "ordered" },
-        { list: "bullet" },
-        { indent: "-1" },
-        { indent: "+1" }
-      ],
-      ["clean"]
-    ]
-  };
-
-  formats = [
-    "header",
-    "bold",
-    "italic",
-    "underline",
-    "strike",
-    "blockquote",
-    "code-block",
-    "list",
-    "bullet",
-    "indent"
-  ];
+  constructor(props) {
+    super(props);
+    this.quillRef = React.createRef();
+    this.modules = {
+      toolbar: {
+        container: [
+          [{ header: [1, 2, false] }],
+          ["bold", "italic", "underline", "strike", "blockquote", "code-block"],
+          [
+            { list: "ordered" },
+            { list: "bullet" },
+            { indent: "-1" },
+            { indent: "+1" }
+          ],
+          ["image"],
+          ["clean"]
+        ],
+        handlers: {
+          image: (image, callback) => {
+            var range = this.quillRef.current.getEditor().getSelection();
+            var value = prompt("請輸入圖片網址。");
+            if (value) {
+              this.quillRef.current
+                .getEditor()
+                .insertEmbed(range.index, "image", value, "user");
+            }
+          }
+        }
+      }
+    };
+    this.formats = [
+      "header",
+      "bold",
+      "italic",
+      "underline",
+      "strike",
+      "blockquote",
+      "code-block",
+      "list",
+      "bullet",
+      "indent",
+      "image"
+    ];
+  }
 
   changeHandler(value) {
     this.props.changed(value);
@@ -42,6 +60,7 @@ class PostEditor extends Component {
       <div style={{ marginBottom: "10px" }}>
         <ReactQuill
           {...this.props}
+          ref={this.quillRef}
           theme="snow"
           value={this.props.value}
           modules={this.modules}
