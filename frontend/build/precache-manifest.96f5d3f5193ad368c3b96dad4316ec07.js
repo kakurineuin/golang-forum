79279a91dde4fec3525d1d68a4a64215f0167b36
Kevin <kakurineuin@gmail.com>
2019-02-17 18:27:40 +0800

    上傳 frontend/build。

---
+++ frontend/build/precache-manifest.96f5d3f5193ad368c3b96dad4316ec07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d38cc85d97c7679b044",
+    "url": "/static/js/main.8d38cc85.chunk.js"
+  },
+  {
+    "revision": "d67812ac820c2c3674ed",
+    "url": "/static/js/1.d67812ac.chunk.js"
+  },
+  {
+    "revision": "8d38cc85d97c7679b044",
+    "url": "/static/css/main.c0d602d9.chunk.css"
+  },
+  {
+    "revision": "d67812ac820c2c3674ed",
+    "url": "/static/css/1.bea94e23.chunk.css"
+  },
+  {
+    "revision": "e3772185c975062456782827a03eba30",
+    "url": "/index.html"
+  }
+];
